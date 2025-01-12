84dd66184ef6fdf80fb4d38c3377279674b775f8
vanshikapal14 <vanshikapal451@gmail.com>
2025-01-12 15:29:46 +0530

    Delete JavaScript Validation and Interactivity directory

--- JavaScript Validation and Interactivity/DynamicErrorMessages.js
+++
@@ -1,7 +0,0 @@
-// ... (within the submit event handler)
-if (!emailRegex.test(email)) {
-    const emailError = document.createElement('p');
-    emailError.textContent = 'Invalid email address';
-    emailError.style.color = 'red';
-    email.parentNode.appendChild(emailError);
-  }
